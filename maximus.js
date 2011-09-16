1f65642e1b3ebf720a5f49a973b262e9be9dba51
Joe Lennon <joe@joelennonie>
2011-09-16 17:43:41 +0100

    Reduced helper functions code to only include IE6 support
    This script should only ever be used in a conditional comment so that
    it is only loaded by IE6. As a result, we don't need cross-browser
    implementations of the helper functions in the script, so I've removed
    them.

--- maximus.js
+++ maximus.js
@@ -2,105 +2,39 @@ var Maximus = {
 	fixedClassName: "mxjs-fixed",
 	/* Helper functions */
 	getWindowSize: function() {
-		var size = {}, w = window, d = document;
-		size.width = w.innerWidth || d.documentElement.clientWidth || d.body.clientWidth || 0;
-		size.height = w.innerHeight || d.documentElement.clientHeight || d.body.clientHeight || 0;
-		return size;
+		return {
+			width: document.documentElement.clientWidth || document.body.clientWidth || 0,
+			height: document.documentElement.clientHeight || document.body.clientHeight || 0
+		}
 	},
-	addEvent: function(obj, type, fn) {
-		if (obj.attachEvent) {
-	    	obj['e'+type+fn] = fn;
-	    	obj[type+fn] = function(){obj['e'+type+fn](window.event);}
-	    	obj.attachEvent('on'+type, obj[type+fn]);
-		} else obj.addEventListener(type, fn, false);
+	addEvent: function(obj, type, fn) {		
+    	obj['e'+type+fn] = fn;
+    	obj[type+fn] = function(){obj['e'+type+fn](window.event);}
+    	obj.attachEvent('on'+type, obj[type+fn]);
 	},
 	removeEvent: function(obj, type, fn) {
-		if (obj.detachEvent) {
-			obj.detachEvent('on'+type, obj[type+fn]);
-			obj[type+fn] = null;
-		} else obj.removeEventListener(type, fn, false);
+		obj.detachEvent('on'+type, obj[type+fn]); obj[type+fn] = null;
 	},
 	getStyle: function(el,styleProp) {
-		if (el.currentStyle) return el.currentStyle[styleProp];
-		else if (window.getComputedStyle) return document.defaultView.getComputedStyle(x,null).getPropertyValue(styleProp);
+		return el.currentStyle[styleProp];
 	},
 	getElementsByClassName: function(className, tag, elm) {
-		if (document.getElementsByClassName) {
-	        getElementsByClassName = function (className, tag, elm) {
-                elm = elm || document;
-                var elements = elm.getElementsByClassName(className),
-					nodeName = (tag)? new RegExp("\\b" + tag + "\\b", "i") : null,
-					returnElements = [],
-					current;
-                for(var i=0, il=elements.length; i<il; i+=1){
-					current = elements[i];
-					if(!nodeName || nodeName.test(current.nodeName)) {
-						returnElements.push(current);
-					}
-                }
-                return returnElements;
-	        };
+		tag = tag || "*"; elm = elm || document;
+		var classes = className.split(" "), classesToCheck = [], current, returnElements = [], match,
+			elements = (tag === "*" && elm.all)? elm.all : elm.getElementsByTagName(tag);
+		for(var k=0, kl=classes.length; k<kl; k+=1) classesToCheck.push(new RegExp("(^|\\s)" + classes[k] + "(\\s|$)"));
+		for(var l=0, ll=elements.length; l<ll; l+=1){
+			current = elements[l]; match = false;
+			for(var m=0, ml=classesToCheck.length; m<ml; m+=1){
+				match = classesToCheck[m].test(current.className); if (!match) break;
+			}
+			if(match) returnElements.push(current);
 		}
-		else if (document.evaluate) {
-			getElementsByClassName = function (className, tag, elm) {
-		        tag = tag || "*";
-		        elm = elm || document;
-		        var classes = className.split(" "),
-	                classesToCheck = "",
-	                xhtmlNamespace = "http://www.w3.org/1999/xhtml",
-	                namespaceResolver = (document.documentElement.namespaceURI === xhtmlNamespace)? xhtmlNamespace : null,
-	                returnElements = [],
-	                elements,
-	                node;
-		        for(var j=0, jl=classes.length; j<jl; j+=1) {
-		        	classesToCheck += "[contains(concat(' ', @class, ' '), ' " + classes[j] + " ')]";
-		        }
-		        try {
-		        	elements = document.evaluate(".//" + tag + classesToCheck, elm, namespaceResolver, 0, null);
-		        }
-		        catch(e) {
-		        	elements = document.evaluate(".//" + tag + classesToCheck, elm, null, 0, null);
-		        }
-		        while((node = elements.iterateNext())) {
-		        	returnElements.push(node);
-		        }
-		        return returnElements;
-			};
-		}
-		else {
-	        getElementsByClassName = function (className, tag, elm) {
-                tag = tag || "*";
-                elm = elm || document;
-                var classes = className.split(" "),
-					classesToCheck = [],
-					elements = (tag === "*" && elm.all)? elm.all : elm.getElementsByTagName(tag),
-					current,
-					returnElements = [],
-					match;
-                for(var k=0, kl=classes.length; k<kl; k+=1){
-	                classesToCheck.push(new RegExp("(^|\\s)" + classes[k] + "(\\s|$)"));
-                }
-                for(var l=0, ll=elements.length; l<ll; l+=1){
-					current = elements[l];
-					match = false;
-					for(var m=0, ml=classesToCheck.length; m<ml; m+=1){
-				        match = classesToCheck[m].test(current.className);
-				        if (!match) {
-				        	break;
-				        }
-					}
-					if (match) {
-						returnElements.push(current);
-					}
-                }
-                return returnElements;
-	        };
-		}
-		return getElementsByClassName(className, tag, elm);
+		return returnElements;
 	},
-	build: function() {
+	init: function() {
 		if(document.documentElement) document.documentElement.style.overflow = "hidden";
-		else if(document.body) document.body.style = "hidden";
+		if(document.body) document.body.style.overflow = "hidden";
 		
 		var fixedEls = Maximus.getElementsByClassName(Maximus.fixedClassName);
 		var i = 0, len = fixedEls.length, dims = Maximus.getWindowSize();
@@ -116,5 +50,5 @@ var Maximus = {
 	}
 }
 
-Maximus.addEvent(window, "load", Maximus.build);
-Maximus.addEvent(window, "resize", Maximus.build);
+Maximus.addEvent(window, "load", Maximus.init);
+Maximus.addEvent(window, "resize", Maximus.init);
