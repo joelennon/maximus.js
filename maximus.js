dcf1f31f755f31f97015636c5b1121e99c2e1be9
Joe Lennon <joe@joelennonie>
2011-09-16 18:18:58 +0100

    Removed class name dependency
    You no longer need to add classes for the script to work - it will
    automatically find any elements with position: fixed and apply the
    relevant changes to those elements. Should now also work in both quirks
    and standards modes in IE6.

--- maximus.js
+++ maximus.js
@@ -1,6 +1,4 @@
 var Maximus = {
-	fixedClassName: "mxjs-fixed",
-	/* Helper functions */
 	getWindowSize: function() {
 		return {
 			width: document.documentElement.clientWidth || document.body.clientWidth || 0,
@@ -18,32 +16,25 @@ var Maximus = {
 	getStyle: function(el,styleProp) {
 		return el.currentStyle[styleProp];
 	},
-	getElementsByClassName: function(className, tag, elm) {
-		tag = tag || "*"; elm = elm || document;
-		var classes = className.split(" "), classesToCheck = [], current, returnElements = [], match,
-			elements = (tag === "*" && elm.all)? elm.all : elm.getElementsByTagName(tag);
-		for(var k=0, kl=classes.length; k<kl; k+=1) classesToCheck.push(new RegExp("(^|\\s)" + classes[k] + "(\\s|$)"));
-		for(var l=0, ll=elements.length; l<ll; l+=1){
-			current = elements[l]; match = false;
-			for(var m=0, ml=classesToCheck.length; m<ml; m+=1){
-				match = classesToCheck[m].test(current.className); if (!match) break;
-			}
-			if(match) returnElements.push(current);
+	getFixedElements: function() {
+		var returnEls = [], els = (document.all) ? document.all : document.getElementsByTagName("*");
+		for(var i=0, len=els.length; i<len; i++) {
+			if(Maximus.getStyle(els[i], "position").toUpperCase() === "FIXED") returnEls.push(els[i]);
 		}
-		return returnElements;
+		return returnEls;
 	},
 	init: function() {
 		if(document.documentElement) document.documentElement.style.overflow = "hidden";
 		if(document.body) document.body.style.overflow = "hidden";
 		
-		var fixedEls = Maximus.getElementsByClassName(Maximus.fixedClassName);
-		var i = 0, len = fixedEls.length, dims = Maximus.getWindowSize();
-		for(;i<len;i++) {
+		var fixedEls = Maximus.getFixedElements();
+		var t, b, l, r, dims = Maximus.getWindowSize();
+		for(var i=0, len=fixedEls.length;i<len;i++) {
 			fixedEls[i].style.position = "absolute";
-			var t = parseInt(Maximus.getStyle(fixedEls[i], "top"), 10),
-				b = parseInt(Maximus.getStyle(fixedEls[i], "bottom"), 10),
-				l = parseInt(Maximus.getStyle(fixedEls[i], "left"), 10),
-				r = parseInt(Maximus.getStyle(fixedEls[i], "right"), 10);
+			t = parseInt(Maximus.getStyle(fixedEls[i], "top"), 10);
+			b = parseInt(Maximus.getStyle(fixedEls[i], "bottom"), 10);
+			l = parseInt(Maximus.getStyle(fixedEls[i], "left"), 10);
+			r = parseInt(Maximus.getStyle(fixedEls[i], "right"), 10);
 			if(t > 0 || b > 0) fixedEls[i].style.height = (dims.height - t - b) + "px";
 			if(!isNaN(l) && !isNaN(r)) fixedEls[i].style.width = (dims.width - l - r) + "px";
 		}
